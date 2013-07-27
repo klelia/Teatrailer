bdcc3b86cdbd235d372ca245fa7929a7d450da3d
klelia <clelia.fradella@gmail.com>
2013-07-27 14:38:16 +0200

    teat_dev0.1-270713

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,4 @@
-var aTeatrailerServices=angular.module('Teat',[]);
+var aTeatrailerServices=angular.module('Teat',['ng-iscroll']);
 
 function listaRouteConfig($routeProvider){
 	$routeProvider.
@@ -10,9 +10,13 @@ function listaRouteConfig($routeProvider){
 		controller: ListaSpettacoli,
 		templateUrl: 'listaspettacoli.html'
 			}).
-		when('/spettacolo/:nid', {
-			controller: VediSpettacolo,
-			templateUrl: 'spettacolo.html'
+	when('/viewg/:cult/:percorso', {
+	    controller: SpettacoliCult,
+	    templateUrl: 'spettacolicult.html'
+			}).
+	when('/spettacolo/:nid', {
+		controller: VediSpettacolo,
+		templateUrl: 'spettacolo.html'
 			}).
 	otherwise({
 		redirectTo: '/'
@@ -22,18 +26,19 @@ function listaRouteConfig($routeProvider){
 aTeatrailerServices.config(listaRouteConfig);
 
 function MenuPage($scope,$http){
+	var firstitem = {'titolo':'In scena','cult':'tutte','percorso':'oggi-in-scena'};
 	var menuitems = [
-	                 {'titolo':'In scena', 'citta':'tutte','percorso':'oggi-in-scena'},
 	                 {'titolo':'Roma', 'citta':'rm','percorso':'oggi-in-scena'},
 	                 {'titolo':'Milano','citta':'mi', 'percorso':'oggi-in-scena'},
 	                 {'titolo':'Altre città','citta':'altre', 'percorso':'oggi-in-scena'}
 	                 ];
 	$scope.menuitems = menuitems;
+	$scope.firstitem = firstitem;
 }
 
 function ListaSpettacoli($scope,$routeParams,$http){
 	//$http.get('json/'+$routeParams.percorso+'.json').success(function(data){
-		$http.jsonp('http://www.teatrailer.it/json/'+$routeParams.citta+'/'+$routeParams.percorso+'?callback=JSON_CALLBACK',{ cache: true}).success(function(data){
+		$http.jsonp('http://www.teatrailer.it/json/'+$routeParams.citta+'/'+$routeParams.percorso+'?callback=JSON_CALLBACK').success(function(data){
 			$scope.lista = data;
 			$scope.lista.nasc = 'prova';
 	}).
@@ -42,9 +47,25 @@ function ListaSpettacoli($scope,$routeParams,$http){
 		$scope.status = status;
 	});
 }
+function SpettacoliCult($scope,$routeParams,$http){
+	//$http.get('json/'+$routeParams.percorso+'.json').success(function(data){
+		$http.jsonp('http://www.teatrailer.it/json/'+$routeParams.cult+'/'+$routeParams.percorso+'?callback=JSON_CALLBACK').success(function(data){
+			$scope.grid = data;
+			$scope.grid.nasc = 'prova';
+			$scope.$parent.myScrollOptions = {
+					snap: true,
+					momentum: false,
+					hScrollbar: false,
+			       };
+	}).
+	error(function(data, status){
+		$scope.data = data || 'Request failed!';
+		$scope.status = status;
+	});
+}
 function VediSpettacolo($scope,$routeParams,$http){
 	//$http.get('json/spett'+$routeParams.nid+'.json').success(function(data){
-	$http.jsonp('http://www.teatrailer.it/json/'+$routeParams.nid+'/spett?callback=JSON_CALLBACK',{ cache: true}).success(function(data){
+	$http.jsonp('http://www.teatrailer.it/json/'+$routeParams.nid+'/spett?callback=JSON_CALLBACK').success(function(data){
 		$scope.nodespe = data;
 		$scope.nodespe.basevideo='http://msh0056.static.video.seewebstorage.it/hoo9Peib/';
 		$scope.nodespe.nasc = 'prova';
